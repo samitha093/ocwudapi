7137b9ee2755418dd0dec7e556828e1231c13e16
samitha lakshan <50348300+samitha093@users.noreply.github.com>
2021-08-29 18:05:50 +0530

    update

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const url = 'mongodb+srv://lakshan:123Samitha@cluster0.inao4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-
-
-// const host = '0.0.0.0';
-// const port = process.env.PORT || 3000;
-
-
 const app = express()
 
 mongoose.connect(url, {useNewUrlParser:true})
@@ -22,12 +16,13 @@ app.use(express.json())
 const alienRouter = require('./routes/products')
 app.use('/aliens',alienRouter)
 
-// app.listen(port, host, function() {
-//   console.log("Server started.......");
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
-app.listen(3000, () => {
-    console.log('Server started')
-})
+// app.listen(3000, () => {
+//     console.log('Server started')
+// })
 
 
