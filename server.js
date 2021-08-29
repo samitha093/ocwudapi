cec188f9ffbe4806fc337dfef625d607ce7d73b9
samitha lakshan <50348300+samitha093@users.noreply.github.com>
2021-08-29 17:53:47 +0530

    host change

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const url = 'mongodb+srv://lakshan:123Samitha@cluster0.inao4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+
+# use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+
 const app = express()
 
 mongoose.connect(url, {useNewUrlParser:true})
@@ -16,8 +22,12 @@ app.use(express.json())
 const alienRouter = require('./routes/products')
 app.use('/aliens',alienRouter)
 
-app.listen(9000, () => {
-    console.log('Server started')
-})
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});
+
+// app.listen(9000, () => {
+//     console.log('Server started')
+// })
 
 
